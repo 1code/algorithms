4723f37d9f7571f8ddc79b84e8dbad1547a202aa
yexi <yexijiang@gmail.com>
2013-05-10 09:40:09 -0400

    add sketch of algorithms

---
+++ src/main/java/algorithm/cc150/chapter1/Question1.java
@@ -0,0 +1,14 @@
+package algorithm.cc150.chapter1;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What if you cannot use additional data structures?
+ * @author yexijiang
+ *
+ */
+public class Question1 {
+	public boolean allUnique(String strings) {
+		
+		return false;
+	}
+}
