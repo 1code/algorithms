3611c139278ac30386a8e1803f2877e0b5cb7b06
yexi <yexijiang@gmail.com>
2013-06-02 14:51:44 -0400

    add chapter 9 question 3

---
+++ src/main/java/algorithm/cc150/chapter9/Question3.java
@@ -0,0 +1,20 @@
+package algorithm.cc150.chapter9;
+
+/**
+ *	A magic index in an array A[0...n-1] is defined to be an index 
+ *	such that A[i] = i. Given a sorted array, write a method to 
+ *	find a magic index, if one exists, in array A.
+ *
+ * 	FOLLOW UP
+ * 	What if the values are not distinct?
+ *
+ */
+public class Question3 {
+	
+	public int findMagicIndex(int[] array) {
+		//	write implement here
+		
+		return 0;
+	}
+
+}
---
+++ src/test/java/algorithm/cc150/chapter9/TestQuestion3.java
@@ -0,0 +1,37 @@
+package algorithm.cc150.chapter9;
+
+import org.junit.Test;
+
+import algorithm.TestBase;
+
+/**
+ *	A magic index in an array A[0...n-1] is defined to be an index 
+ *	such that A[i] = i. Given a sorted array, write a method to 
+ *	find a magic index, if one exists, in array A.
+ *
+ * 	FOLLOW UP
+ * 	What if the values are not distinct?
+ *
+ */
+public class TestQuestion3 extends TestBase {
+
+	private Question3 question;
+	
+	@Override
+	protected void initInstance() {
+		question = new Question3();
+	}
+
+	@Override
+	@Test
+	public void testPosCase() {
+		
+	}
+
+	@Override
+	@Test
+	public void testNegCase() {
+		
+	}
+
+}
