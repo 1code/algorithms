533f27b65a4c79a4e8d35c411b350d3df0050891
yexi <yexijiang@gmail.com>
2013-06-01 20:26:18 -0400

    add test cases for chapter9 question 1

--- src/test/java/algorithm/cc150/chapter9/TestQuestion1.java
+++ src/test/java/algorithm/cc150/chapter9/TestQuestion1.java
@@ -1,5 +1,7 @@
 package algorithm.cc150.chapter9;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import algorithm.TestBase;
@@ -21,7 +23,20 @@ public class TestQuestion1 extends TestBase {
 	@Override
 	@Test
 	public void testPosCase() {
+		assertEquals(0, question.numOfWays(0));
+		assertEquals(1, question.numOfWays(1));
+		assertEquals(2, question.numOfWays(2));
+		assertEquals(3, question.numOfWays(3));
+		assertEquals(6, question.numOfWays(4));
+		assertEquals(11, question.numOfWays(5));
+		assertEquals(20, question.numOfWays(6));
+		assertEquals(37, question.numOfWays(7));
+		assertEquals(68, question.numOfWays(8));
+		assertEquals(125, question.numOfWays(9));
 		
+		assertEquals(421132538567l, question.numOfWays(45));
+		assertEquals(4819661885417l, question.numOfWays(49));
+		assertEquals(3927900252338210l, question.numOfWays(60));
 	}
 
 	@Override
