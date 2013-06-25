f177c2042b4841804f1ab49b7f7b1053394ee4fd
yexi <yexijiang@gmail.com>
2013-06-25 09:47:16 -0400

    add chapter 2 question5

---
+++ src/main/java/algorithm/cc150/chapter2/Question5.java
@@ -0,0 +1,14 @@
+package algorithm.cc150.chapter2;
+
+/**
+ * You have two numbers represented by a linked list, where each node contains a
+ * single digit. The digits are stored in reverse order, such that the 1s digit
+ * is at the head of the list. Write a function that adds the two numbers and
+ * return the sum as a linked list.
+ * 
+ * 
+ */
+public class Question5 {
+  
+  //  this question is available at leetcode, question Add Two Numbers
+}
