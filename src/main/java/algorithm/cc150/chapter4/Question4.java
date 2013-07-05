d04996eddbbec858f4484a75675007250e91748b
yexi <yexijiang@gmail.com>
2013-07-05 09:54:49 -0400

    add chapter 4 question 4

--- src/main/java/algorithm/cc150/chapter4/Question4.java
+++ src/main/java/algorithm/cc150/chapter4/Question4.java
@@ -10,5 +10,34 @@ public class Question4 {
 
   // this question is available at leetcode, question Populating Next Right
   // Pointers in Each Node I and II
+  public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+      val = x;
+    }
+  }
+
+  public void connect(TreeLinkNode root) {
+    // Start typing your Java solution below
+    // DO NOT write main() function
+    populate(root, null);
+  }
+
+  private void populate(TreeLinkNode node, TreeLinkNode parent) {
+    if (node == null) {
+      return;
+    }
+    if (node.left != null) {
+      node.left.next = node.right;
+    }
+    if (node.right != null && node.next != null) {
+      node.right.next = node.next.left;
+    }
+
+    populate(node.right, node);
+    populate(node.left, node);
+  }
 
 }
