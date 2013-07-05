1cca3e28bd2ee4864f4e30e74762036464e7320f
yexi <yexijiang@gmail.com>
2013-07-04 21:55:41 -0400

    add chapter 4 question 1

--- src/main/java/algorithm/cc150/chapter4/Question1.java
+++ src/main/java/algorithm/cc150/chapter4/Question1.java
@@ -9,5 +9,32 @@ package algorithm.cc150.chapter4;
 public class Question1 {
 
   // this question is available at leetcode, question Balanced Binary Tree
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    // Start typing your Java solution below
+    // DO NOT write main() function
+    return check(root) >= 0;
+  }
+
+  private int check(TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
+    int left = check(node.left);
+    int right = check(node.right);
+    if (left < 0 || right < 0 || Math.abs(left - right) > 1) {
+      return -1;
+    } else {
+      return Math.max(left, right) + 1;
+    }
+  }
 }
