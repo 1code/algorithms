93e98b53516cf1c5542f508ce258934d38842fa2
yexi <yexijiang@gmail.com>
2013-07-05 10:17:15 -0400

    add chapter 4 question 4

--- src/main/java/algorithm/cc150/chapter4/Question4.java
+++ src/main/java/algorithm/cc150/chapter4/Question4.java
@@ -22,22 +22,41 @@ public class Question4 {
   public void connect(TreeLinkNode root) {
     // Start typing your Java solution below
     // DO NOT write main() function
-    populate(root, null);
+    populate(root);
   }
 
-  private void populate(TreeLinkNode node, TreeLinkNode parent) {
+  private void populate(TreeLinkNode node) {
     if (node == null) {
       return;
     }
+    TreeLinkNode needNext = null;
     if (node.left != null) {
-      node.left.next = node.right;
-    }
-    if (node.right != null && node.next != null) {
-      node.right.next = node.next.left;
+      needNext = node.left;
+      if (node.right != null) {
+        node.left.next = node.right;
+      }
     }
 
-    populate(node.right, node);
-    populate(node.left, node);
+    if (node.right != null) {
+      needNext = node.right;
+    }
+
+    if (needNext != null && node.next != null) {
+      TreeLinkNode nextParent = node.next;
+      while (nextParent != null) {
+        if (nextParent.left != null) {
+          needNext.next = nextParent.left;
+          break;
+        }
+        if (nextParent.right != null) {
+          needNext.next = nextParent.right;
+          break;
+        }
+        nextParent = nextParent.next;
+      }
+    }
+    populate(node.right);
+    populate(node.left);
   }
-
+  
 }
