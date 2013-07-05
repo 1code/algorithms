a77d9ae6e76a1033e160c88fdbba45030c39a2d0
yexi <yexijiang@gmail.com>
2013-07-04 22:14:34 -0400

    add chapter 4 question 2

--- src/main/java/algorithm/cc150/chapter4/Question2.java
+++ src/main/java/algorithm/cc150/chapter4/Question2.java
@@ -1,6 +1,9 @@
 package algorithm.cc150.chapter4;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Given a directed graph, design an algorithm to find out whether there is a
@@ -15,7 +18,11 @@ public class Question2 {
     private LinkedList<Integer> edges[];
 
     public DirectedGraph(int nodeSize) {
-      this.edges = (LinkedList<Integer>[]) new LinkedList[nodeSize];
+      this.v = nodeSize;
+      this.edges = (LinkedList<Integer>[]) new LinkedList[v];
+      for (int i = 0; i < v; ++i) {
+        this.edges[i] = new LinkedList<Integer>();
+      }
     }
 
     public void addEdge(int v, int w) {
@@ -41,8 +48,31 @@ public class Question2 {
 
   public boolean isRouteBetween(DirectedGraph g, int nodeIdx1, int nodeIdx2) {
     // write implementation here
+      return reachable(g, nodeIdx1, nodeIdx2) || reachable(g, nodeIdx2, nodeIdx1);
+    }
 
-    return true;
+    private boolean reachable(DirectedGraph g, int from, int to) {
+      boolean[] visited = new boolean[g.V()];
+      Arrays.fill(visited, false);
+      Stack<Integer> stack = new Stack<Integer>();
+      stack.push(from);
+      while (!stack.isEmpty()) {
+        int cur = stack.pop();
+        visited[cur] = true;
+        Iterator<Integer> neighbors = g.adj(cur).iterator();
+        while (neighbors.hasNext()) {
+          int neighbor = neighbors.next();
+          if (!visited[neighbor]) {
+            if (neighbor == to) {
+              return true;
+            }
+            stack.push(neighbor);
+          }
+        }
+      }
+ 
+      return false;
   }
 
 }
+
--- src/test/java/algorithm/cc150/chapter4/TestQuestion2.java
+++ src/test/java/algorithm/cc150/chapter4/TestQuestion2.java
@@ -1,8 +1,12 @@
 package algorithm.cc150.chapter4;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import algorithm.TestBase;
+import algorithm.cc150.chapter4.Question2.DirectedGraph;
 
 /**
  * Given a directed graph, design an algorithm to find out whether there is a
@@ -21,15 +25,43 @@ public class TestQuestion2 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-    // TODO Auto-generated method stub
-
+    DirectedGraph g = new DirectedGraph(9);
+    g.addEdge(0, 2);
+    g.addEdge(2, 1);
+    g.addEdge(1, 0);
+    g.addEdge(1, 3);
+    g.addEdge(3, 1);
+    g.addEdge(1, 4);
+    g.addEdge(4, 3);
+    g.addEdge(5, 4);
+    g.addEdge(6, 7);
+    g.addEdge(7, 8);
+    g.addEdge(6, 8);
+    
+    assertTrue(question.isRouteBetween(g, 0, 5));
+    assertTrue(question.isRouteBetween(g, 5, 0));
+    assertTrue(question.isRouteBetween(g, 6, 7));
+    assertTrue(question.isRouteBetween(g, 6, 8));
   }
 
   @Override
   @Test
   public void testNegCase() {
-    // TODO Auto-generated method stub
-
+    DirectedGraph g = new DirectedGraph(9);
+    g.addEdge(0, 2);
+    g.addEdge(2, 1);
+    g.addEdge(1, 0);
+    g.addEdge(1, 3);
+    g.addEdge(3, 1);
+    g.addEdge(1, 4);
+    g.addEdge(4, 3);
+    g.addEdge(5, 4);
+    g.addEdge(6, 7);
+    g.addEdge(7, 8);
+    g.addEdge(6, 8);
+    
+    assertFalse(question.isRouteBetween(g, 1, 6));
+    assertFalse(question.isRouteBetween(g, 5, 6));
   }
 
 }
