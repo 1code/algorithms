d0df42792c42415571c6cddc915c2483b76a5193
yexi <yexijiang@gmail.com>
2013-07-07 14:00:06 -0400

    add chapter 5 question 3

--- src/main/java/algorithm/cc150/chapter7/Question3.java
+++ src/main/java/algorithm/cc150/chapter7/Question3.java
@@ -20,9 +20,21 @@ public class Question3 {
    * @param y4 y coordinate of end point of line 2
    * @return
    */
-  public boolean isIntersect(int x1, int y1, int x2, int y2, int x3, int y3,
-      int x4, int y4) {
+  public boolean isIntersect(double x1, double y1, double x2, double y2, 
+      double x3, double y3, double x4, double y4) {
     // write implementation here
-    return true;
+    // identify with cross check
+    // check whether (x3, y3) and (x4, y4) are on different side of line 1
+    double sign1 = (x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3);
+    double sign2 = (x1 - x2) * (y1 - y4) - (y1 - y2) * (x1 - x4);
+    if (sign1 * sign2 >= 0) {
+      return false;
+    }
+    
+    // chechk whether (x1, y1) and (x2, y2) are on different side of line 2
+    double sign3 = (x3 - x4) * (y3 - y1) - (y3 - y4) * (x3 - x1);
+    double sign4 = (x3 - x4) * (y3 - y2) - (y3 - y4) * (x3 - x2);
+    
+    return sign3 * sign4 < 0;
   }
 }
--- src/test/java/algorithm/cc150/chapter7/TestQuestion3.java
+++ src/test/java/algorithm/cc150/chapter7/TestQuestion3.java
@@ -1,5 +1,8 @@
 package algorithm.cc150.chapter7;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import algorithm.TestBase;
@@ -21,15 +24,49 @@ public class TestQuestion3 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-    // TODO Auto-generated method stub
-
+    double x11 = 0;
+    double y11 = 0;
+    double x12 = 2;
+    double y12 = 2;
+    double x13 = 0;
+    double y13 = 1;
+    double x14 = 2;
+    double y14 = 0;
+    assertTrue(question.isIntersect(x11, y11, x12, y12, x13, y13, x14, y14));
+    
+    double x21 = 0.5;
+    double y21 = 0.5;
+    double x22 = 2;
+    double y22 = 2;
+    double x23 = 0;
+    double y23 = 1;
+    double x24 = 2;
+    double y24 = 1;
+    assertTrue(question.isIntersect(x21, y21, x22, y22, x23, y23, x24, y24));
   }
 
   @Override
   @Test
   public void testNegCase() {
-    // TODO Auto-generated method stub
-
+    double x11 = 1;
+    double y11 = 1;
+    double x12 = 2;
+    double y12 = 2;
+    double x13 = 0;
+    double y13 = 1;
+    double x14 = 2;
+    double y14 = 0;
+    assertFalse(question.isIntersect(x11, y11, x12, y12, x13, y13, x14, y14));
+    
+    double x21 = 1;
+    double y21 = 1;
+    double x22 = 2;
+    double y22 = 2;
+    double x23 = 0;
+    double y23 = 1;
+    double x24 = 1;
+    double y24 = 1;
+    assertFalse(question.isIntersect(x21, y21, x22, y22, x23, y23, x24, y24));
   }
 
 }
