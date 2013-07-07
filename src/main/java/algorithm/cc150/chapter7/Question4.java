a85e355fed3433b18f289bd80eb10c1d074d837d
yexi <yexijiang@gmail.com>
2013-07-07 15:23:11 -0400

    add chapter 7 question 4

--- src/main/java/algorithm/cc150/chapter7/Question4.java
+++ src/main/java/algorithm/cc150/chapter7/Question4.java
@@ -9,17 +9,60 @@ public class Question4 {
 
   public int mul(int a, int b) {
     // write implementation here
-    return 0;
+    int signb = 1;
+    if (b < 0) {
+      b = -b; 
+      signb = -1;
+    }
+    int res = 0;
+    for (int i = 0; i < b; ++i) {
+      res += a;
+    }
+    if (signb < 0) {
+      return -res;
+    }
+    else {
+      return res;
+    }
   }
 
   public int subtract(int a, int b) {
     // write implementation here
-    return 0;
+    int sign = 1;
+    if (b < 0) {
+      b = -b;
+      sign = -1;
+    }
+    for (int i = 0; i < b; ++i) {
+      if (sign == 1) {
+        a += -1;
+      }
+      else {
+        ++a;
+      }
+    }
+    return a;
   }
 
   public int div(int a, int b) {
     // write implementation here
-    return 0;
+    int res = 0;
+    int sign = 1;
+    if (mul(a, b) < 0) {
+      sign = -1;
+    }
+    if (a < 0) {
+      a = -a;
+    }
+    if (b < 0) {
+      b = -b;
+    }
+    while (a >= b) {
+      a -= b;
+      ++res;
+    }
+    return mul(sign, res);
   }
 
 }
+
--- src/test/java/algorithm/cc150/chapter7/TestQuestion4.java
+++ src/test/java/algorithm/cc150/chapter7/TestQuestion4.java
@@ -1,5 +1,9 @@
 package algorithm.cc150.chapter7;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
 import org.junit.Test;
 
 import algorithm.TestBase;
@@ -21,7 +25,26 @@ public class TestQuestion4 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-
+    int a1 = -5;
+    int b1 = 2;
+    assertEquals(a1 - b1, question.subtract(a1, b1));
+    assertEquals(a1 * b1, question.mul(a1, b1));
+    assertEquals(a1 / b1, question.div(a1, b1));
+    
+    int a2 = 30;
+    int b2 = -4;
+    assertEquals(a2 - b2, question.subtract(a2, b2));
+    assertEquals(a2 * b2, question.mul(a2, b2));
+    assertEquals(a2 / b2, question.div(a2, b2));
+    
+    Random rnd = new Random();
+    for (int c = 0; c < 10; ++c) {
+      int a = rnd.nextInt(1000) - 500;
+      int b = rnd.nextInt(1000) - 500;
+      assertEquals(a - b, question.subtract(a, b));
+      assertEquals(a * b, question.mul(a, b));
+      assertEquals(a / b, question.div(a, b));
+    }
   }
 
   @Override
