00afb3c8a06beecd1bec100dd89e0cafb143d2ea
yexi <yexijiang@gmail.com>
2013-07-07 16:23:00 -0400

    add chapter 5 question 5

--- src/main/java/algorithm/cc150/chapter7/Question5.java
+++ src/main/java/algorithm/cc150/chapter7/Question5.java
@@ -10,7 +10,6 @@ import java.util.List;
  * 
  */
 public class Question5 {
-
   /**
    * 
    * @param x1 The x coordinate of the upper left corner of the first square.
@@ -21,15 +20,39 @@ public class Question5 {
    * @param y3 The y coordinate of the upper left corner of the second square.
    * @param x4 The x coordinate of the lower right corner of the second square.
    * @param y4 The y coordinate of the lower right corner of the second square.
-   * @return An array list containing four integer, including the x coordinate
-   *         of the start point of the line, the y coordinate of the start point
-   *         of the line, the x coordinate of the end point of the line, and the
-   *         y coordinate of the end point of the line.
+   * @return An array list containing two integers, the slope and the intercept of the line. 
+   *                if line is vertical (x = c) and horizontal (y = c), return the constant only, 
+   *                if two square coincides, return the x, y of the points.
    */
-  public List<Integer> cutInHalf(int x1, int y1, int x2, int y2, int x3,
-      int y3, int x4, int y4) {
-
-    return new ArrayList<Integer>();
+  public double[] cutInHalf(double x1, double y1, double x2, double y2, double x3,
+      double y3, double x4, double y4) {
+    double[] arr;
+    double m1x = (x1 + x2) / 2;
+    double m1y = (y1 + y2) / 2;
+    double m2x = (x3 + x4) / 2;
+    double m2y = (y3 + y4) / 2;
+    
+    if (m1x == m2x && m1y == m2y) {  // any line cross the middle point
+      arr = new double[2];
+      arr[0] = m1x;
+      arr[1] = m1y;
+    }
+    else if (m1y == m2y) {  // horizontal line, y = c
+      arr = new double[1];
+      arr[0] = m1y;
+    }
+    else if (m1x == m2x) {  // vertical line, x = c
+      arr = new double[1];
+      arr[0] = m1x;
+    }
+    else {
+      arr = new double[2];
+      arr[0] = (m1y - m2y) / (m1x - m2x);
+      arr[1] = m1y - arr[0] * m1x;
+    }
+    return arr;
   }
 
 }
+
+
--- src/test/java/algorithm/cc150/chapter7/TestQuestion5.java
+++ src/test/java/algorithm/cc150/chapter7/TestQuestion5.java
@@ -1,6 +1,7 @@
 package algorithm.cc150.chapter7;
 
 import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 
 import algorithm.TestBase;
 
@@ -22,14 +23,58 @@ public class TestQuestion5 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-    // TODO Auto-generated method stub
-
+    // y = 0
+    double x11 = 0;
+    double y11 = 1;
+    double x12 = 1;
+    double y12 = -1;
+    double x13 = 2;
+    double y13 = 1;
+    double x14 = 3;
+    double y14 = -1;
+    double[] arr1 = new double[] {0};
+    assertArrayEquals(arr1, question.cutInHalf(x11, y11, x12, y12, x13, y13, x14, y14), 0.00001);
+    
+    // x = 1;
+    double x21 = 0;
+    double y21 = 1;
+    double x22 = 2;
+    double y22 = 0;
+    double x23 = 0;
+    double y23 = 3;
+    double x24 = 2;
+    double y24 = 2;
+    double[] arr2 = new double[] {1};
+    assertArrayEquals(arr2, question.cutInHalf(x21, y21, x22, y22, x23, y23, x24, y24), 0.00001);
+    
+    // coincides
+    double x31 = 0;
+    double y31 = 1;
+    double x32 = 1;
+    double y32 = -1;
+    double x33 = 0;
+    double y33 = 1;
+    double x34 = 1;
+    double y34 = -1;
+    double[] arr3 = new double[] {0.5, 0};
+    assertArrayEquals(arr3, question.cutInHalf(x31, y31, x32, y32, x33, y33, x34, y34), 0.00001);
+    
+    // y = -0.5x - 1
+    double x41 = 0;
+    double y41 = -1;
+    double x42 = 4;
+    double y42 = -3;
+    double x43 = 5;
+    double y43 = -3.5;
+    double x44 = 7;
+    double y44 = -4.5;
+    double[] arr4 = new double[] {-0.5, -1};
+    assertArrayEquals(arr4, question.cutInHalf(x41, y41, x42, y42, x43, y43, x44, y44), 0.00001);
   }
 
   @Override
   @Test
   public void testNegCase() {
-    // TODO Auto-generated method stub
 
   }
 
