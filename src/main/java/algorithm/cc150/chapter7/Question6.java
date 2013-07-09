a221d4437b65899895d65001909f14348f42a143
yexi <yexijiang@gmail.com>
2013-07-09 09:03:50 -0400

    add chapter 7 question 6

--- src/main/java/algorithm/cc150/chapter7/Question6.java
+++ src/main/java/algorithm/cc150/chapter7/Question6.java
@@ -1,6 +1,8 @@
 package algorithm.cc150.chapter7;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Given a two-dimensional graph with points on it, find a line which passes the
@@ -20,18 +22,40 @@ public class Question6 {
   }
 
   public static class Line {
-    public enum Type {NORMAL, VERTICAL, HORIZONTAL};
-    public Type type;
-    public double a;
-    public double b;
-    public Line(Type type) {
-      this.type = type;
+    private static final double epsilon = 0.000001;
+    private double slope;
+    private double intercept;
+    private boolean infiniteSlope = false;
+
+    public Line(Point p1, Point p2) {
+      if (Math.abs(p1.x - p2.x) < epsilon) {
+        this.infiniteSlope = true;
+      }
+      else {
+       this.slope = (p2.y - p1.y) / (p2.x - p1.x);
+       this.intercept = p1.y - this.slope * p1.x;
+      }
     }
+
     public boolean equals(Object other) {
       if (other instanceof Line) {
-        
+        Line otherLine = (Line)other;
+        if (this.infiniteSlope != otherLine.infiniteSlope) {
+          return false;
+        }
+        if (this.infiniteSlope == true) { // both has infinite slope
+          return true;
+        }
+        else {
+          return this.slope == otherLine.slope && this.intercept == otherLine.intercept;
+        }
       }
+        
       return false;
+    }
+    
+    public int hashCode() {
+      return (int)(intercept * 17);
     }
   }
 
@@ -42,15 +66,34 @@ public class Question6 {
    *                if line is vertical (x = c) and horizontal (y = c), return the constant only, 
    *                if two square coincides, return the x, y of the points.
    */
-  public double[] findLine(List<Point> points) {
+  public Line findLine(List<Point> points) {
     // write implementation here
-    double[] arr = null;
+    Map<Line, Integer> map = new HashMap<Line, Integer>();
+    int bestCount = 0;
+    Line bestLine = null;
+    for (int i = 0; i < points.size(); ++i) {
+      for (int j = 0; j < points.size(); ++j) {
+        if (i == j) {
+          continue;
+        }
+        Point p1 = points.get(i);
+        Point p2 = points.get(j);
+        Line line = new Line(p1, p2);
+        Integer count = map.get(line);
+        if (count == null) {
+          count = 0;
+          map.put(line, count);
+        }
+        map.put(line, count + 1);
+        if (count > bestCount) {
+          bestCount = count;
+          bestLine = line;
+        }
+      }
+    }
     
-    return arr;
+    return bestLine;
   }
 
 }
-
-
-
 
--- src/test/java/algorithm/cc150/chapter7/TestQuestion6.java
+++ src/test/java/algorithm/cc150/chapter7/TestQuestion6.java
@@ -1,8 +1,16 @@
 package algorithm.cc150.chapter7;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.Test;
 
 import algorithm.TestBase;
+import algorithm.cc150.chapter7.Question6.Line;
+import algorithm.cc150.chapter7.Question6.Point;
 
 /**
  * Given a two-dimensional graph with points on it, find a line which passes the
@@ -21,15 +29,37 @@ public class TestQuestion6 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-    // TODO Auto-generated method stub
-
+    List<Point> points = new ArrayList<Point>();
+    Random rnd = new Random();
+    
+    for (int i = 0; i < 10; ++i) {
+      points.add(new Point(i, i));
+    }
+    // add random points
+    for (int i = 0; i < 10; ++i) {
+      points.add(new Point(rnd.nextInt(100), rnd.nextInt(100)));
+    }
+    
+    assertEquals(new Line(new Point(5, 5), new Point(6, 6)), question.findLine(points));
+    
+    
+    // test lines with infinite slope
+    points.clear();
+    for (int i = 0; i < 10; ++i) {
+      points.add(new Point(i, 5));
+    }
+    // add random points
+    for (int i = 0; i < 10; ++i) {
+      points.add(new Point(rnd.nextInt(100), rnd.nextInt(100)));
+    }
+    
+    assertEquals(new Line(new Point(5, 5), new Point(6, 5)), question.findLine(points));
   }
 
   @Override
   @Test
   public void testNegCase() {
-    // TODO Auto-generated method stub
-
+    //  no negative points
   }
 
 }
