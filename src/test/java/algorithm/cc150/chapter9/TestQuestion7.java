be3c9e962e4b58c0fae460fffe4a4b6e2f168a97
yexi <yexijiang@gmail.com>
2013-07-10 09:59:05 -0400

    add chapter 9 question 7

--- src/test/java/algorithm/cc150/chapter9/TestQuestion7.java
+++ src/test/java/algorithm/cc150/chapter9/TestQuestion7.java
@@ -20,33 +20,63 @@ public class TestQuestion7 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-    char[][] screen1 = new char[][] { "rrrrrrrrrr".toCharArray(),
-        "rwwwwwwwwr".toCharArray(), "rwrrwrrrwr".toCharArray(),
-        "rwrrrrrrwr".toCharArray(), "rwwwrrrrwr".toCharArray(),
-        "rwrwwwwwwr".toCharArray(), "rwwwrrrrrw".toCharArray(),
-        "rwwrrrrrww".toCharArray(), "rwwwrrrrrr".toCharArray(),
+    char[][] screen0 = new char[][] {
+      "rrrrr".toCharArray(),
+      "rwwwr".toCharArray(),
+      "rwwrr".toCharArray(),
+      "rwrrr".toCharArray()
+    };
+    char[][] result0 = new char[][] {
+        "rrrrr".toCharArray(),
+        "rrrrr".toCharArray(),
+        "rrrrr".toCharArray(),
+        "rrrrr".toCharArray()
+    };
+    question.fill(screen0, 1, 1, 'r');
+    assertArrayEquals(result0, screen0);
+    
+    char[][] screen1 = new char[][] { 
+        "rrrrrrrrrr".toCharArray(),
+        "rwwwwwwwwr".toCharArray(), 
+        "rwrrwrrrwr".toCharArray(),
+        "rwrrrrrrwr".toCharArray(), 
+        "rwwwrrrrwr".toCharArray(),
+        "rwrwwwwwwr".toCharArray(), 
+        "rwwwrrrrrw".toCharArray(),
+        "rwwrrrrrww".toCharArray(), 
+        "rwwwrrrrrr".toCharArray(),
         "rrrwwwwrrr".toCharArray() };
 
-    char[][] result1 = new char[][] { "rrrrrrrrrr".toCharArray(),
-        "rrrrrrrrrr".toCharArray(), "rrrrrrrrrr".toCharArray(),
-        "rrrrrrrrrr".toCharArray(), "rrrrrrrrrr".toCharArray(),
-        "rrrrrrrrrr".toCharArray(), "rrrrrrrrrr".toCharArray(),
-        "rrrrrrrrrr".toCharArray(), "rrrrrrrrrr".toCharArray(),
+    char[][] result1 = new char[][] { 
+        "rrrrrrrrrr".toCharArray(),
+        "rrrrrrrrrr".toCharArray(), 
+        "rrrrrrrrrr".toCharArray(),
+        "rrrrrrrrrr".toCharArray(), 
+        "rrrrrrrrrr".toCharArray(),
+        "rrrrrrrrrr".toCharArray(), 
+        "rrrrrrrrrw".toCharArray(),
+        "rrrrrrrrww".toCharArray(), 
+        "rrrrrrrrrr".toCharArray(),
         "rrrrrrrrrr".toCharArray() };
 
-    char[][] copy1 = null;
+    char[][] copy1 = new char[screen1.length][screen1[0].length];;
     cloneArray(copy1, screen1);
     question.fill(copy1, 1, 1, 'r');
     assertArrayEquals(result1, copy1);
 
-    char[][] result2 = new char[][] { "rrrrrrrrrr".toCharArray(),
-        "rbbbbbbbbr".toCharArray(), "rbrrbrrrbr".toCharArray(),
-        "rbrrrrrrbr".toCharArray(), "rbbbrrrrbr".toCharArray(),
-        "rbrbbbbbbr".toCharArray(), "rbbbrrrrrb".toCharArray(),
-        "rbbrrrrrbb".toCharArray(), "rbbbrrrrrr".toCharArray(),
+    char[][] result2 = new char[][] { 
+        "rrrrrrrrrr".toCharArray(),
+        "rbbbbbbbbr".toCharArray(), 
+        "rbrrbrrrbr".toCharArray(),
+        "rbrrrrrrbr".toCharArray(), 
+        "rbbbrrrrbr".toCharArray(),
+        "rbrbbbbbbr".toCharArray(), 
+        "rbbbrrrrrw".toCharArray(),
+        "rbbrrrrrww".toCharArray(), 
+        "rbbbrrrrrr".toCharArray(),
         "rrrbbbbrrr".toCharArray() };
 
-    char[][] copy2 = null;
+    char[][] copy2 = new char[screen1.length][screen1[0].length];
     cloneArray(copy2, screen1);
     question.fill(copy2, 1, 1, 'b');
     assertArrayEquals(result2, copy2);
@@ -60,9 +90,6 @@ public class TestQuestion7 extends TestBase {
   }
 
   private static void cloneArray(char[][] dest, char[][] source) {
-    if (dest == null) {
-      dest = new char[source.length][source[0].length];
-    }
     for (int i = 0; i < source.length; ++i) {
       dest[i] = Arrays.copyOf(source[i], source[i].length);
     }
