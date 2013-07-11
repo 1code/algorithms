bf62bc2cacddcb3324309b0e366c74ba43dbc9a2
yexi <yexijiang@gmail.com>
2013-07-11 11:32:29 -0400

    add chapter 11 question 1

--- src/main/java/algorithm/cc150/chapter11/Question1.java
+++ src/main/java/algorithm/cc150/chapter11/Question1.java
@@ -8,5 +8,31 @@ package algorithm.cc150.chapter11;
 public class Question1 {
 
   // this question is available at leetcode, question Merge Sorted Array
+  public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+          // Start typing your Java solution below
+          // DO NOT write main() function
+      int i = m - 1;
+      int j = n - 1;
+      int k = A.length - 1;
+      while (k >= 0) {
+        if (i >= 0 && j >= 0) {
+          if (A[i] > B[j]) {
+            A[k--] = A[i--];
+          }
+          else {
+            A[k--] = B[j--];
+          }
+        }
+        else if (i >= 0) {
+          A[k--] = A[i--];
+        }
+        else if (j >= 0) {
+          A[k--] = B[j--];
+        }
+      }
+    }
+  }
+
 
 }
