88ad2c29769b2bd6d60eb46ecb78c2368677b434
yexi <yexijiang@gmail.com>
2013-07-11 14:04:26 -0400

    add chapter 11 question 3

--- src/main/java/algorithm/cc150/chapter11/Question3.java
+++ src/main/java/algorithm/cc150/chapter11/Question3.java
@@ -13,5 +13,55 @@ public class Question3 {
 
   // this question is available at leetcode, question Search in Rotated Sorted
   // Array
+  public class Solution {
+    public int search(int[] A, int target) {
+          // Start typing your Java solution below
+          // DO NOT write main() function
+      return search(A, target, 0, A.length - 1);
+    }
+
+    private int search(int[] A, int target, int left, int right) {
+      if (left > right || right < 0) {
+        return -1;
+      }
+      int mid = (left + right) / 2;
+      int vMid = A[mid];
+      if (target == vMid) {
+        return mid;
+      }
+      
+      if (A[left] < vMid) { // left in order
+        if (A[left] <= target && target < vMid) { // in range of left
+          return search(A, target, left, mid - 1);
+        }
+        else {
+          return search(A, target, mid + 1, right);
+        }
+      }
+      else if (A[left] > vMid) {  // right in order
+        if (vMid <= target && target <= A[right]) { // in range of right
+          return search(A, target, mid + 1, right);
+        }
+        else {
+          return search(A, target, left, mid - 1);
+        }
+      }
+      else {
+        if (A[mid] != A[right]) {
+          return search(A, target, mid + 1, right);
+        }
+        else {
+          int idx = search(A, target, left, mid - 1);
+          if (idx != -1) {
+            idx = search(A, target, mid + 1, right);
+          }
+          return idx;
+        }
+      }
+      
+    }
+  }
+
+
 
 }
