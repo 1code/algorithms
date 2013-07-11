9441ed13f8c43cc04f3b642bae4b20b32ec65397
yexi <yexijiang@gmail.com>
2013-07-11 11:24:21 -0400

    add chapter 9 question 10

--- src/main/java/algorithm/cc150/chapter9/Question10.java
+++ src/main/java/algorithm/cc150/chapter9/Question10.java
@@ -1,6 +1,8 @@
 package algorithm.cc150.chapter9;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * You have a stack of n boxes, with widths w_i, heights h_i and depths d_i. The
@@ -14,19 +16,59 @@ public class Question10 {
 
   public long talleast(List<Box> boxes) {
     // write implementaion here
+    boolean[] used = new boolean[boxes.size()];
+    int prevWidth = Integer.MAX_VALUE;
+    int prevDepth = Integer.MAX_VALUE;
+    return add(used, boxes, prevWidth, prevDepth);
+  }
 
-    return 0;
+  private long add(boolean[] used, List<Box> boxes,
+      int prevWidth, int prevDepth) {
+    long maxHeight = 0;
+    
+    for (int i = 0; i < boxes.size(); ++i) {
+      Box box = boxes.get(i);
+      if (!used[i]) {
+        used[i] = true;
+        // width as height
+        if (prevWidth >= box.height && prevDepth >= box.depth
+            || prevWidth >= box.depth && prevDepth >= box.height) {
+          long height = add(used, boxes, box.height, box.depth);
+          if (height + box.width > maxHeight) {
+            maxHeight = height + box.width;
+          }
+        }
+        // depth as height
+        if (prevWidth >= box.width && prevDepth >= box.height
+            || prevWidth >= box.height && prevDepth >= box.width) {
+          long height = add(used, boxes, box.width, box.height);
+          if (height + box.depth > maxHeight) {
+            maxHeight = height + box.depth;
+          }
+        }
+        // height as height
+        if (prevWidth >= box.width && prevDepth >= box.depth
+            || prevWidth >= box.depth && prevDepth >= box.width) {
+          long height = add(used, boxes, box.width, box.depth);
+          if (height + box.height > maxHeight) {
+            maxHeight = height + box.height;
+          }
+        }
+        used[i] = false;
+      }
+    }
+    return maxHeight;
   }
 
   public static class Box {
     public int width;
-    public int height;
     public int depth;
+    public int height;
 
-    public Box(int width, int height, int depth) {
+    public Box(int width, int depth, int height) {
       this.width = width;
-      this.height = height;
       this.depth = depth;
+      this.height = height;
     }
   }
 
--- src/test/java/algorithm/cc150/chapter9/TestQuesiton10.java
+++ src/test/java/algorithm/cc150/chapter9/TestQuesiton10.java
@@ -1,8 +1,16 @@
 package algorithm.cc150.chapter9;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.Test;
 
 import algorithm.TestBase;
+import algorithm.cc150.chapter9.Question10.Box;
 
 /**
  * You have a stack of n boxes, with widths w_i, heights h_i and depths d_i. The
@@ -24,15 +32,38 @@ public class TestQuesiton10 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-    // TODO Auto-generated method stub
+    // one box
+    Box box1 = new Box(1, 2, 3);
+    List<Box> boxList1 = new ArrayList<Box>();
+    boxList1.add(box1);
+    assertEquals(3, question.talleast(boxList1));
 
+    // two boxes
+    Box[] boxes2 = new Box[] { new Box(1, 1, 3), new Box(2, 1, 3) };
+    List<Box> boxList2 = new ArrayList<Box>();
+    boxList2.addAll(Arrays.asList(boxes2));
+    assertEquals(6, question.talleast(boxList2));
+
+    // three boxes
+    Box[] boxes3 = new Box[] { new Box(3, 5, 2), new Box(2, 1, 1),
+        new Box(5, 3, 1) };
+    List<Box> boxList3 = new ArrayList<Box>();
+    boxList3.addAll(Arrays.asList(boxes3));
+    assertEquals(12, question.talleast(boxList3));
+
+    // ten boxes, for performance testing
+    Random rnd = new Random();
+    List<Box> boxList4 = new ArrayList<Box>();
+    for (int b = 0; b < 16; ++b) {
+      boxList4.add(new Box(rnd.nextInt(10), rnd.nextInt(10), rnd.nextInt(10)));
+    }
+    System.out.println(question.talleast(boxList4));
   }
 
   @Override
   @Test
   public void testNegCase() {
-    // TODO Auto-generated method stub
-
+    // no negative case
   }
 
 }
