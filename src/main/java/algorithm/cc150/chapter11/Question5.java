2300f5a16a91da554b6e77a507306e81769c839f
yexi <yexijiang@gmail.com>
2013-07-12 10:03:06 -0400

    add chapter 11 question 5

--- src/main/java/algorithm/cc150/chapter11/Question5.java
+++ src/main/java/algorithm/cc150/chapter11/Question5.java
@@ -7,12 +7,55 @@ package algorithm.cc150.chapter11;
  * EXAMPLE Input: find "ball" in {"at", "", "", "", "ball", "", "", "car", "",
  * "", "dad", "", ""} Output: 4
  */
+// O(1) space, O(n) time
 public class Question5 {
 
-  public int findString(String[] strs) {
+  public int findString(String[] strs, String target) {
     // write implementation here
+    return find(strs, target, 0, strs.length - 1);
+  }
 
-    return 0;
+  private int find(String[] strs, String target, int left, int right) {
+    if (left > right || left < 0 || right >= strs.length) {
+      return -1;
+    }
+    int mid = (left + right) / 2;
+    if (strs[mid].equals(target)) {
+      return mid;
+    }
+    if (strs[mid].isEmpty()) {
+      int prev = mid - 1;
+      int next = mid + 1; 
+      while (true) {
+        if (prev < left && next > right) { // cannot find non-empty in range
+          return -1;
+        }
+        else if (prev >= left && !strs[prev].isEmpty()) {
+          mid = prev;
+          break;
+        }
+        else if (next <= right && !strs[next].isEmpty()) {
+          mid = next;
+          break;
+        }
+        --prev;
+        ++next;
+      } 
+    }
+    
+    if (strs[mid].equals(target)) {
+      return mid;
+    }
+    else if (target.compareTo(strs[mid]) < 0) {
+      return find(strs, target, left, mid - 1);
+    }
+    else {
+      return find(strs, target, mid + 1, right);
+    }
   }
 
 }
+
+
+
+
--- src/test/java/algorithm/cc150/chapter11/TestQuestion5.java
+++ src/test/java/algorithm/cc150/chapter11/TestQuestion5.java
@@ -1,5 +1,7 @@
 package algorithm.cc150.chapter11;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import algorithm.TestBase;
@@ -23,15 +25,39 @@ public class TestQuestion5 extends TestBase {
   @Override
   @Test
   public void testPosCase() {
-    // TODO Auto-generated method stub
+    String[] strs1 = new String[] {
+        "", "", "ab", "ac", "", "ad", "", "", "ae", "ba", "bb", "bc", "", "bd"
+    };
+    String target1 = "ae";
+    assertEquals(8, question.findString(strs1, target1));
+    String target2 = "ab";
+    assertEquals(2, question.findString(strs1, target2));
+    String target3 = "ac";
+    assertEquals(3, question.findString(strs1, target3));
+    String target4 = "bb";
+    assertEquals(10, question.findString(strs1, target4));
+    String target5 = "cc";
+    assertEquals(-1, question.findString(strs1, target5));
 
+    String[] strs2 = new String[] {
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+    };
+    String target6 = "j";
+    assertEquals(9, question.findString(strs2, target6));
+    String target7 = "a";
+    assertEquals(0, question.findString(strs2, target7));
+    String target8 = "e";
+    assertEquals(4, question.findString(strs2, target8));
+    String target9 = "j";
+    assertEquals(9, question.findString(strs2, target9));
+    String target10 = "1";
+    assertEquals(-1, question.findString(strs2, target10));
   }
 
   @Override
   @Test
   public void testNegCase() {
-    // TODO Auto-generated method stub
-
+    // no negative case
   }
 
 }
