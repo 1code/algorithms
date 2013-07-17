3cd61aaaf8325d64d98bef48a13a100766adac17
yexi <yexijiang@gmail.com>
2013-07-17 12:15:35 -0400

    chapter 17 question 2, fix

--- src/main/java/algorithm/cc150/chapter18/Question2.java
+++ src/main/java/algorithm/cc150/chapter18/Question2.java
@@ -11,8 +11,8 @@ public class Question2 {
   public void perfectShuffle(int[] cards) {
     // write implementation here
     Random rnd = new Random();
-    for (int i = 0; i < cards.length; ++i) {
-      int idx = rnd.nextInt(i + 1);
+    for (int i = 1; i < cards.length; ++i) {
+      int idx = rnd.nextInt(i);
       int tmp = cards[idx];
       cards[idx] = cards[i];
       cards[i] = tmp;
