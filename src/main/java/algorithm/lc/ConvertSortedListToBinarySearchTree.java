231d07fae77b71bdbd7d3b2625d0ce646d366840
yexi <yexijiang@gmail.com>
2013-07-23 09:36:46 -0400

    add lc, Convert Sorted List to Binary Search Tree

---
+++ src/main/java/algorithm/lc/ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,63 @@
+package algorithm.lc;
+
+/**
+ * Given a singly linked list where elements are sorted in ascending order,
+ * convert it to a height balanced BST.
+ * 
+ */
+// O(1) space, O(n) time. 
+public class ConvertSortedListToBinarySearchTree {
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  // Convert left subtree, current node to root, and then convert right subtree
+  public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+      // Start typing your Java solution below
+      // DO NOT write main() function
+      int size = 0;
+      ListNode[] wrapper = new ListNode[1];
+      wrapper[0] = head;
+      while (wrapper[0] != null) {
+        ++size;
+        wrapper[0] = wrapper[0].next;
+      }
+      wrapper[0] = head;
+      return sortedListToBST(wrapper, 0, size - 1);
+    }
+
+    // build left tree, current root , and then right tree
+    private TreeNode sortedListToBST(ListNode[] wrapper, int min, int max) {
+      if (min <= max) { // non-empty
+        int mid = (min + max) / 2;
+        TreeNode leftTree = sortedListToBST(wrapper, min, mid - 1);
+        TreeNode curRoot = new TreeNode(wrapper[0].val);
+        curRoot.left = leftTree;
+        wrapper[0] = wrapper[0].next;
+        curRoot.right = sortedListToBST(wrapper, mid + 1, max);
+        return curRoot;
+      } else {
+        return null;
+      }
+    }
+  }
+
+}
