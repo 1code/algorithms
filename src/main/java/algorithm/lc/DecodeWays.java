574bf905abfa8e5eddf5b5ba017863f0b8a30965
yexi <yexijiang@gmail.com>
2013-07-29 08:40:06 -0400

    add lc, Decode Ways

---
+++ src/main/java/algorithm/lc/DecodeWays.java
@@ -0,0 +1,50 @@
+package algorithm.lc;
+
+/**
+ * A message containing letters from A-Z is being encoded to numbers using the
+ * following mapping:
+ * 
+ * 'A' -> 1 'B' -> 2 ... 'Z' -> 26 Given an encoded message containing digits,
+ * determine the total number of ways to decode it.
+ * 
+ * For example, Given encoded message "12", it could be decoded as "AB" (1 2) or
+ * "L" (12).
+ * 
+ * The number of ways decoding "12" is 2.
+ * 
+ */
+// O(n) space, O(n) time
+public class DecodeWays {
+
+  public class Solution {
+    // Use 1-d DP
+    public int numDecodings(String s) {
+      // Start typing your Java solution below
+      // DO NOT write main() function
+      if (s == null || s.length() == 0) {
+        return 0;
+      }
+      int[] ways = new int[s.length() + 1];
+      ways[0] = 1;
+      int prev = 0;
+      for (int i = 1; i <= s.length(); ++i) {
+        int cur = s.charAt(i - 1) - '0';
+        int value = prev * 10 + cur;
+        if (cur == 0 && (prev > 2 || prev == 0)) {
+          return 0; // invalid code
+        }
+        if (value == 20 || value == 10) {
+          ways[i] = ways[i - 2];
+        } else if (10 < value && value <= 26) {
+          ways[i] = ways[i - 1] + ways[i - 2];
+        } else {
+          ways[i] = ways[i - 1];
+        }
+        prev = cur;
+      }
+
+      return ways[s.length()];
+    }
+  }
+
+}
