e5ba602b8586c7829fd10c69f4df899a5304a849
yexi <yexijiang@gmail.com>
2013-07-30 08:42:59 -0400

    add lc, Remove Duplicate from Sorted List

---
+++ src/main/java/algorithm/lc/RemoveDuplicateFromSortedList.java
@@ -0,0 +1,47 @@
+package algorithm.lc;
+
+/**
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ * 
+ * For example, Given 1->1->2, return 1->2. Given 1->1->2->3->3, return 1->2->3.
+ * 
+ */
+// O(1) space, O(n) time
+public class RemoveDuplicateFromSortedList {
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+
+  public class Solution {
+    // three pointer to point to previous node, current node, and next node
+    public ListNode deleteDuplicates(ListNode head) {
+      // Start typing your Java solution below
+      // DO NOT write main() function
+      if (head == null) {
+        return null;
+      }
+      ListNode pre = head;
+      ListNode cur = head.next;
+      while (cur != null) {
+        ListNode next = cur.next;
+        if (cur.val == pre.val) {
+          pre.next = next;
+          cur = next;
+        } else {
+          pre = cur;
+          cur = next;
+        }
+      }
+      return head;
+    }
+  }
+
+}
