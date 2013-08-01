8737571310e21b980dd98b8e5ea01530412c4e91
yexi <yexijiang@gmail.com>
2013-08-01 10:52:25 -0400

    add lc, Search for a Range

--- src/main/java/algorithm/lc/DivideTwoIntegers.java
+++ src/main/java/algorithm/lc/DivideTwoIntegers.java
@@ -4,37 +4,42 @@ package algorithm.lc;
  * Divide two integers without using multiplication, division and mod operator.
  * 
  */
+// O(1) space, O(lgn) time
 public class DivideTwoIntegers {
 
   public class Solution {
+    // use right shift operation
     public int divide(int dividend, int divisor) {
-      // Start typing your Java solution below
-      // DO NOT write main() function
-      int symbol = (dividend < 0) != (divisor < 0) ? -1 : 1;
-      int tmpDividend = Math.abs(dividend);
-      int tmpDivisor = Math.abs(divisor);
-
-      // special cases
-      if (divisor == 0) // invalid
-        return Integer.MAX_VALUE;
-      if (divisor == Integer.MIN_VALUE)
-        return dividend == Integer.MIN_VALUE ? 1 : 0;
-      if (dividend == Integer.MIN_VALUE)
-        return symbol * 1 + divide(dividend - symbol * divisor, divisor);
-
-      int result = 0;
-      int product = tmpDivisor;
-      while (tmpDivisor <= tmpDividend) {
-        int tmpResult = 1;
-        while (tmpDividend - product >= product) {
-          product <<= 1;
-          tmpResult <<= 1;
-        }
-        result += tmpResult;
-        tmpDividend -= product;
-        product = tmpDivisor;
+          // Start typing your Java solution below
+          // DO NOT write main() function
+          
+      int sign = (dividend < 0) != (divisor < 0)? -1 : 1;
+      if (divisor == 0) {
+        return 0;
       }
-      return symbol * result;
+      if (divisor == Integer.MIN_VALUE) {
+        return dividend == Integer.MIN_VALUE ? 1 : 0;
+      }
+      if (dividend == Integer.MIN_VALUE) {
+        return sign + divide(dividend - (sign > 0? divisor : -divisor), divisor);
+      }
+      dividend = Math.abs(dividend);
+      divisor = Math.abs(divisor);
+      
+      int res = 0;
+      
+      while (dividend >= divisor) {
+        int tmp = 1;
+        int product = divisor;
+        while (dividend - product >= product) {
+          product <<= 1;
+          tmp <<= 1;
+        }
+        res += tmp;
+        dividend -= product;
+      }
+      
+      return sign > 0 ? res : -res;
     }
   }
 
---
+++ src/main/java/algorithm/lc/SearchForARange.java
@@ -0,0 +1,58 @@
+package algorithm.lc;
+
+/**
+ * Given a sorted array of integers, find the starting and ending position of a
+ * given target value.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * If the target is not found in the array, return [-1, -1].
+ * 
+ * For example, Given [5, 7, 7, 8, 8, 10] and target value 8, return [3, 4].
+ * 
+ */
+// O(1) space, O(lgn) time
+public class SearchForARange {
+
+  public class Solution {
+    // use binary search twice
+    public int[] searchRange(int[] A, int target) {
+      // Start typing your Java solution below
+      // DO NOT write main() function
+      int start = -1, end = -1;
+      int left = 0, right = A.length - 1;
+
+      // search for start
+      while (left <= right) {
+        int mid = (left + right) / 2;
+        if (target == A[mid]) {
+          start = (start == -1 ? mid : Math.min(start, mid));
+          right = mid - 1; // continue search left part
+        } else if (target > A[mid]) {
+          left = mid + 1;
+        } else { // if target < A[mid]
+          right = mid - 1;
+        }
+      }
+
+      left = 0;
+      right = A.length - 1;
+      while (left <= right) {
+        int mid = (left + right) / 2;
+        if (target == A[mid]) {
+          end = (end == -1 ? mid : Math.max(mid, end));
+          left = mid + 1; // continue search right
+        } else if (target > A[mid]) {
+          left = mid + 1;
+        } else { // target < A[mid]
+          right = mid - 1;
+        }
+      }
+
+      int[] res = { start, end };
+      return res;
+    }
+
+  }
+
+}
