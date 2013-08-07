e3654f2dd14e2dc3dcc72991f8fbafe357e4f83f
yexi <yexijiang@gmail.com>
2013-08-07 14:31:21 -0400

    add lc, Sort Colors

---
+++ src/main/java/algorithm/lc/SortColors.java
@@ -0,0 +1,51 @@
+package algorithm.lc;
+
+/**
+ * Given an array with n objects colored red, white or blue, sort them so that
+ * objects of the same color are adjacent, with the colors in the order red,
+ * white and blue.
+ * 
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, white,
+ * and blue respectively.
+ * 
+ * Note: You are not suppose to use the library's sort function for this
+ * problem.
+ * 
+ * Follow up: A rather straight forward solution is a two-pass algorithm using
+ * counting sort. First, iterate the array counting number of 0's, 1's, and 2's,
+ * then overwrite array with total number of 0's, then 1's and followed by 2's.
+ * 
+ * Could you come up with an one-pass algorithm using only constant space?
+ * 
+ */
+// O(1) space, O(n) time
+public class SortColors {
+
+  public class Solution {
+    // two pointers
+    public void sortColors(int[] A) {
+      // Start typing your Java solution below
+      // DO NOT write main() function
+      int redPos = 0;
+      int bluePos = A.length - 1;
+      int i = 0;
+      while (i <= bluePos) {
+        if (A[i] == 0) {
+          int tmp = A[redPos];
+          A[redPos] = A[i];
+          A[i] = tmp;
+          ++redPos;
+          ++i;
+        } else if (A[i] == 2) {
+          int tmp = A[bluePos];
+          A[bluePos] = A[i];
+          A[i] = tmp;
+          --bluePos;
+        } else {
+          ++i;
+        }
+      }
+    }
+  }
+
+}
