d5cbd165200ab359b070f984fcdbfc7164c712d1
yexi <yexijiang@gmail.com>
2013-08-10 14:13:42 -0400

    add lc, Recover Binary Search Tree

--- src/main/java/algorithm/lc/GrayCode.java
+++ src/main/java/algorithm/lc/GrayCode.java
@@ -2,22 +2,52 @@ package algorithm.lc;
 
 import java.util.ArrayList;
 
+/**
+ * The gray code is a binary numeral system where two successive values differ
+ * in only one bit.
+ * 
+ * Given a non-negative integer n representing the total number of bits in the
+ * code, print the sequence of gray code. A gray code sequence must begin with
+ * 0.
+ * 
+ * For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+ * 
+ * 00 - 0 
+ * 01 - 1 
+ * 11 - 3 
+ * 10 - 2 
+ * 
+ * Note: For a given n, a gray code sequence is not
+ * uniquely defined.
+ * 
+ * For example, [0,2,3,1] is also a valid gray code sequence according to the
+ * above definition.
+ * 
+ * For now, the judge is able to judge based on one instance of gray code
+ * sequence. Sorry about that.
+ * 
+ */
+// O(2^n) space, O(2^n) time
 public class GrayCode {
-  
+
   public static class Solution {
+    // build list of n from list of n - 1
+    // 1. get the reverse of the list
+    // 2. add suffix 0 to original list, add suffix 1 to reversed list
+    // 3. concatenate two lists
     public static ArrayList<Integer> grayCode(int n) {
-          // Start typing your Java solution below
-          // DO NOT write main() function
+      // Start typing your Java solution below
+      // DO NOT write main() function
       ArrayList<StringBuilder> res = new ArrayList<StringBuilder>();
       if (n == 0) {
         ArrayList<Integer> zero = new ArrayList<Integer>();
         zero.add(0);
         return zero;
       }
-      
+
       res.add(new StringBuilder().append(0));
       res.add(new StringBuilder().append(1));
-      
+
       // generate gray codes
       for (int i = 0; i < n - 1; ++i) {
         ArrayList<StringBuilder> reflectListWithOne = new ArrayList<StringBuilder>();
@@ -44,11 +74,11 @@ public class GrayCode {
         grayCodes.add(val);
         System.out.println(val);
       }
-      
+
       return grayCodes;
     }
   }
-  
+
   public static void main(String[] args) {
     Solution.grayCode(3);
   }
---
+++ src/main/java/algorithm/lc/RecoverBinarySearchTree.java
@@ -0,0 +1,95 @@
+package algorithm.lc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Two elements of a binary search tree (BST) are swapped by mistake.
+ * 
+ * Recover the tree without changing its structure.
+ * 
+ * Note: A solution using O(n) space is pretty straight forward. Could you
+ * devise a constant space solution?
+ * 
+ */
+public class RecoverBinarySearchTree {
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  // O(n) space, O(nlgn) time
+  public class Solution {
+    public void recoverTree(TreeNode root) {
+      // Start typing your Java solution below
+      // DO NOT write main() function
+      List<TreeNode> nodeList = new ArrayList<TreeNode>();
+      List<Integer> valList = new ArrayList<Integer>();
+
+      inorder(root, nodeList, valList);
+      Collections.sort(valList);
+      for (int i = 0; i < nodeList.size(); ++i) {
+        nodeList.get(i).val = valList.get(i);
+      }
+    }
+
+    private void inorder(TreeNode node, List<TreeNode> nodeList,
+        List<Integer> valList) {
+      if (node == null) {
+        return;
+      }
+      inorder(node.left, nodeList, valList);
+      nodeList.add(node);
+      valList.add(node.val);
+      inorder(node.right, nodeList, valList);
+    }
+  }
+  
+  // O(1) space, O(n) time
+  // extend the inorder traversal, use two pointers to track the nodes that needs to be swaped
+  public class SolutionConstantSpace {
+    public void recoverTree(TreeNode root) {
+          // Start typing your Java solution below
+          // DO NOT write main() function
+      Wrapper first = new Wrapper(null);
+      Wrapper second = new Wrapper(null);
+      Wrapper pre = new Wrapper(null);
+      
+      inorder(root, first, second, pre);
+      // swap two nodes
+      int val = first.node.val;
+      first.node.val = second.node.val;
+      second.node.val = val;
+    }
+    
+    private void inorder(TreeNode node, Wrapper first, Wrapper second, Wrapper pre) {
+      if (node == null) {
+        return;
+      }
+      inorder(node.left, first, second, pre);
+      if (pre.node != null && node.val < pre.node.val) {
+        second.node = node;
+        if (first.node == null) {
+          first.node = pre.node;
+        }
+      }
+      pre.node = node;
+      inorder(node.right, first, second, pre);
+    }
+    
+    public class Wrapper {
+      TreeNode node;
+      public Wrapper(TreeNode node) {
+        this.node = node;
+      }
+    }
+  }
+
+}
