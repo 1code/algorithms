a4cca4635a3011b23f6dfb6cf8b16d567f575607
yexi <yexijiang@gmail.com>
2013-08-14 10:14:50 -0400

    add lc, Construct Binary Tree From Inorder and Postorder Traversal

---
+++ src/main/java/algorithm/lc/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,59 @@
+package algorithm.lc;
+
+/**
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ * 
+ * Note: You may assume that duplicates do not exist in the tree.
+ * 
+ */
+public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
+
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  // use postorder to find the root, and use root in inorder to get left and
+  // right subtree
+  public static class Solution {
+    public static TreeNode buildTree(int[] inorder, int[] postorder) {
+      return buildTree(inorder, 0, inorder.length - 1, postorder, 0,
+          postorder.length - 1);
+    }
+
+    public static TreeNode buildTree(int[] inorder, int inFrom, int inTo,
+        int[] postorder, int postFrom, int postTo) {
+      if (postFrom > postTo) { // no available root
+        return null;
+      }
+
+      TreeNode root = new TreeNode(postorder[postTo]);
+
+      int rootInorderIdx = -1;
+      for (int x = inFrom; x <= inTo; x++)
+        if (inorder[x] == postorder[postTo]) {
+          rootInorderIdx = x;
+          break;
+        }
+
+      root.left = buildTree(inorder, inFrom, rootInorderIdx - 1, postorder,
+          postFrom, postFrom + (rootInorderIdx - inFrom - 1));
+      root.right = buildTree(inorder, rootInorderIdx + 1, inTo, postorder,
+          postFrom + (rootInorderIdx - inFrom), postTo - 1);
+      
+      return root;
+    }
+  }
+  
+  public static void main(String[] args) {
+    int[] inorder = {2, 1};
+    int[] postorder = {2, 1};
+    Solution.buildTree(inorder, postorder);
+  }
+
+}
