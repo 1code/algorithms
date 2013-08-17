13734ff8b3488b2d2ffd85fa34cacea83dedd37c
yexi <yexijiang@gmail.com>
2013-08-17 17:34:54 -0400

    fix PalindromePartition II

--- src/main/java/algorithm/lc/PalindromePartitionII.java
+++ src/main/java/algorithm/lc/PalindromePartitionII.java
@@ -14,28 +14,26 @@ public class PalindromePartitionII {
 
   // DP, res[i] records the min cuts between i and n
   // also the palindrome isPalindrome is used to keep track that whether s[i..j] is palindrome
-  public class Solution {
-    public int minCut(String s) {
-      // Start typing your C/C++ solution below
-      // DO NOT write int main() function
-      int n = s.length();
-      int[] res = new int[n + 1]; // keep min cut from i to n
-      for (int i = 0; i < res.length; ++i) {
-        res[i] = i - 1;
-      }
+  class Solution {  
+    public int minCut(String s) {  
+      // Start typing your C/C++ solution below  
+      // DO NOT write int main() function  
+      int n = s.length();  
+      int[] res = new int[n + 1]; // keep min number of partitions from in range s[i..n]
+      for(int i = 0 ; i < res.length; ++i){  
+        res[i] = n - i;  
+      }  
       boolean[][] isPalindrome = new boolean[n][n];
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <= i; ++j) {
-          if (s.charAt(i) == s.charAt(j)
-              && (i - j < 2 || isPalindrome[j + 1][i - 1])) { // s[1-j] is
-                                                              // palindrome
-            isPalindrome[j][i] = true;
-            res[i + 1] = Math.min(res[i + 1], res[j] + 1);
-          }
-        }
-      }
-      return res[n];
-    }
-  };
+      for(int i = n - 1; i >= 0; --i){  
+        for(int j = i; j < n ; ++j){  
+          if(s.charAt(i) == s.charAt(j) && (j - i < 2 || isPalindrome[i + 1][j - 1])){  // s[1-j] is palindrome
+            isPalindrome[i][j] = true;  
+            res[i] = Math.min(res[i], res[j + 1] + 1);  
+          }  
+        }  
+      }  
+      return res[0] - 1;  
+    }  
+  };  
 
 }
