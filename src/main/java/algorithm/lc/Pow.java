f4cefda19ce65f4ebef7c9b1e56d6a65f95ebf64
yexi <yexijiang@gmail.com>
2013-08-19 09:29:56 -0400

    add another solution for pow

--- src/main/java/algorithm/lc/Pow.java
+++ src/main/java/algorithm/lc/Pow.java
@@ -4,9 +4,9 @@ package algorithm.lc;
  * Implement pow(x, n)
  * 
  */
-// O(1) space, O(logn) time
 public class Pow {
 
+  //O(1) space, O(logn) time, if length of number is fixed, it is constant running time
   public class Solution {
     public double pow(double x, int n) {
           // Start typing your Java solution below
@@ -24,6 +24,27 @@ public class Pow {
         n >>= 1;
       }
       return isPositive? 1.0 / res : res;
+    }
+  }
+  
+  // O(1) space, O(logn) time
+  public class Solution2 {
+    public double pow(double x, int n) {
+          // Start typing your Java solution below
+          // DO NOT write main() function
+      boolean isPositive = n > 0;
+      n = Math.abs(n);
+      double res = getPow(x, n);
+      return isPositive? res : 1.0 / res;
+    }
+    
+    private double getPow(double x, int n) {
+      if (n == 0) {
+        return 1;
+      }
+      double half = getPow(x, n / 2);
+      double res = half * half * (n % 2 == 0? 1 : x);
+      return res;
     }
   }
 }
