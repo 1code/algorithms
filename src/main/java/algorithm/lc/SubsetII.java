260f737083c154a3fbc9454239347e238aaf8b7f
yexi <yexijiang@gmail.com>
2013-08-22 09:04:18 -0400

    add recursive solution for SubsetsII

--- src/main/java/algorithm/lc/SubsetII.java
+++ src/main/java/algorithm/lc/SubsetII.java
@@ -3,6 +3,8 @@ package algorithm.lc;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -43,6 +45,42 @@ public class SubsetII {
         subset(curIdx + 1, copy, res, num);
         // not include cur
         subset(curIdx + 1, curRes, res, num);
+      }
+    }
+  }
+  
+  public class SolutionRecursive {
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+          // Start typing your Java solution below
+          // DO NOT write main() function
+      Set<ArrayList<Integer>> res = new HashSet<ArrayList<Integer>>();
+      Arrays.sort(S);
+      Pair first = new Pair(0, new ArrayList<Integer>());
+      Queue<Pair> queue = new LinkedList<Pair>();
+      queue.offer(first);
+      while (!queue.isEmpty()) {
+        Pair cur = queue.poll();
+        int idx = cur.curIdx;
+        ArrayList<Integer> list = cur.list;
+        if (idx == S.length) {
+          res.add(list);
+        }
+        else {
+          queue.offer(new Pair(idx + 1, list));
+          ArrayList<Integer> copy = new ArrayList<Integer>(list);
+          copy.add(S[idx]);
+          queue.offer(new Pair(idx + 1, copy));
+        }
+      }
+      return new ArrayList<ArrayList<Integer>>(res);
+    }
+    
+    public class Pair {
+      int curIdx;
+      ArrayList<Integer> list;
+      public Pair(int idx, ArrayList<Integer> list) {
+        this.curIdx = idx;
+        this.list = list;
       }
     }
   }
