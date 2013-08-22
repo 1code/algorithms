71e36cd848cccf18ab7add748d871a6e3fae32c1
yexi <yexijiang@gmail.com>
2013-08-21 21:50:47 -0400

    improve Permutation Sequence

--- src/main/java/algorithm/lc/PermutationSequence.java
+++ src/main/java/algorithm/lc/PermutationSequence.java
@@ -20,46 +20,44 @@ public class PermutationSequence {
 
   public class Solution {
     public String getPermutation(int n, int k) {
-      // Start typing your Java solution below
-      // DO NOT write main() function
+          // Start typing your Java solution below
+          // DO NOT write main() function
       int[] num = new int[n];
       for (int i = 0; i < n; ++i) {
         num[i] = i + 1;
-      }
-
+      } 
       for (int i = 1; i < k; ++i) {
         next(num);
       }
       StringBuilder sb = new StringBuilder();
-      for (int v : num) {
-        sb.append(v);
+      for (int i : num) {
+        sb.append(i);
       }
       return sb.toString();
     }
-
+    
     private void next(int[] num) {
-      int lastSmallIdx = num.length - 2;
-      while (num[lastSmallIdx] >= num[lastSmallIdx + 1]) {
-        --lastSmallIdx;
+      if (num.length == 1) {
+        return;
       }
-      if (lastSmallIdx == -1) {
-        Arrays.sort(num);
-        return; // no next permutation
+      int lastSmaller = num.length - 2;
+      while (num[lastSmaller] >= num[lastSmaller + 1]) {
+        --lastSmaller;
+        if (lastSmaller < 0) {
+          return;
+        }
       }
-      int lastElementLargerIdx = num.length - 1;
-      while (num[lastElementLargerIdx] <= num[lastSmallIdx]) {
-        --lastElementLargerIdx;
+      int lastBigger = num.length - 1;
+      while (num[lastBigger] <= num[lastSmaller]) {
+        --lastBigger;
       }
-      swap(num, lastElementLargerIdx, lastSmallIdx);
-      ++lastSmallIdx;
+      swap(num, lastSmaller++, lastBigger);
       int last = num.length - 1;
-      while (lastSmallIdx < last) {
-        swap(num, lastSmallIdx, last);
-        ++lastSmallIdx;
-        --last;
+      while (lastSmaller < last) {
+        swap(num, lastSmaller++, last--);
       }
     }
-
+    
     private void swap(int[] num, int first, int second) {
       int tmp = num[first];
       num[first] = num[second];
