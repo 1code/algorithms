4ffde170eb3a6113b1ede718563a95f0c4244db1
yexi <yexijiang@gmail.com>
2013-08-23 10:02:09 -0400

    improve Divide Two Integers

--- src/main/java/algorithm/lc/DivideTwoIntegers.java
+++ src/main/java/algorithm/lc/DivideTwoIntegers.java
@@ -7,39 +7,36 @@ package algorithm.lc;
 // O(1) space, O(lgn) time
 public class DivideTwoIntegers {
 
+  // O(1) space, O(lgn) time
   public class Solution {
-    // use right shift operation
     public int divide(int dividend, int divisor) {
           // Start typing your Java solution below
           // DO NOT write main() function
-          
-      int sign = (dividend < 0) != (divisor < 0)? -1 : 1;
       if (divisor == 0) {
         return 0;
       }
+      int sign = (dividend > 0 != divisor > 0)? -1 : 1;
       if (divisor == Integer.MIN_VALUE) {
-        return dividend == Integer.MIN_VALUE ? 1 : 0;
-      }
+        return dividend == Integer.MIN_VALUE? 1 : 0;
+      } 
       if (dividend == Integer.MIN_VALUE) {
-        return sign + divide(dividend - (sign > 0? divisor : -divisor), divisor);
+        return sign + divide(dividend - (divisor < 0? divisor : -divisor), divisor);
       }
       dividend = Math.abs(dividend);
       divisor = Math.abs(divisor);
       
       int res = 0;
-      
       while (dividend >= divisor) {
-        int tmp = 1;
+        int pow = 1;
         int product = divisor;
         while (dividend - product >= product) {
           product <<= 1;
-          tmp <<= 1;
+          pow <<= 1;
         }
-        res += tmp;
+        res += pow;
         dividend -= product;
       }
-      
-      return sign > 0 ? res : -res;
+      return sign > 0? res : -res;
     }
   }
 
