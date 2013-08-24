c5f26c32030548e109e6d256219af7cd4aa6b70e
yexi <yexijiang@gmail.com>
2013-08-24 14:24:19 -0400

    improve Multiply Strings

--- src/main/java/algorithm/lc/MultiplyStrings.java
+++ src/main/java/algorithm/lc/MultiplyStrings.java
@@ -20,10 +20,10 @@ public class MultiplyStrings {
       int[] res = new int[num1.length() + num2.length()];
       Arrays.fill(res, 0);
       
-      for (int i = num1.length() - 1; i >= 0; --i) {
+      for (int i = num2.length() - 1; i >= 0; --i) {
         int carry = 0, tmp;
-        for (int j = num2.length() - 1; j >=0; --j) {
-          tmp = carry + res[i + j + 1] + (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+        for (int j = num1.length() - 1; j >=0; --j) {
+          tmp = carry + res[i + j + 1] + (num1.charAt(j) - '0') * (num2.charAt(i) - '0');
           res[i + j + 1] = tmp % 10;
           carry = tmp / 10;
         }
@@ -41,5 +41,5 @@ public class MultiplyStrings {
       return sb.toString();
     }
   }
-
+  
 }
