6b0c5871ffb61930c63670764386184ac436c602
yexi <yexijiang@gmail.com>
2013-08-28 09:38:06 -0400

    fix LongestPalindromeSubstring

--- src/main/java/algorithm/lc/LongestPalindromeSubstring.java
+++ src/main/java/algorithm/lc/LongestPalindromeSubstring.java
@@ -6,38 +6,32 @@ package algorithm.lc;
  * palindromic substring.
  * 
  */
-// O(1) space, O(n^2) time
 public class LongestPalindromeSubstring {
 
   public class Solution {
+    // O(1) space, O(n^2) time
     // expand from each of the 2N - 1 centers
     public String longestPalindrome(String s) {
-      // Start typing your Java solution below
-      // DO NOT write main() function
-      String longest = "";
-      if (s.length() == 0) {
-        return longest;
-      }
-
-      for (int i = 0; i < s.length() - 1; ++i) {
-        String str1 = expand(s, i, i);
-        if (str1.length() > longest.length()) {
-          longest = str1;
-        }
-        String str2 = expand(s, i, i + 1); // center lies between characters
-        if (str2.length() > longest.length()) {
-          longest = str2;
+          // Start typing your Java solution below
+          // DO NOT write main() function
+      String res = "";
+      for (int i = 0; i < s.length(); ++i) {
+        String sub1 = expand(s, i, i);
+        res = res.length() >= sub1.length()? res : sub1;
+        if (i < s.length() - 1) {
+          String sub2 = expand(s, i, i + 1);
+          res = res.length() >= sub2.length()? res : sub2;
         }
       }
-      return longest;
+      return res;
     }
-
-    private String expand(String s, int i, int j) {
-      while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
-        --i;
-        ++j;
+    
+    private String expand(String s, int start, int end) {
+      while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+        --start;
+        ++end;
       }
-      return s.substring(i + 1, j);
+      return s.substring(start + 1, end);
     }
   }
   
