6c77a14a553a26dfd56d3c08eb6429acc9fa310c
yexi <yexijiang@gmail.com>
2013-08-29 09:44:24 -0400

    improve Largest Rectangle in Histogram

--- src/main/java/algorithm/lc/LargestRectangleInHistogram.java
+++ src/main/java/algorithm/lc/LargestRectangleInHistogram.java
@@ -1,5 +1,7 @@
 package algorithm.lc;
 
+import java.util.Stack;
+
 
 /**
  * Given n non-negative integers representing the histogram's bar height where
@@ -18,40 +20,33 @@ package algorithm.lc;
 public class LargestRectangleInHistogram {
   
   public class Solution {
-    // for each bin, find the distance to left boundary and distance to right boundary
     public int largestRectangleArea(int[] height) {
           // Start typing your Java solution below
           // DO NOT write main() function
-      int max = 0;
-      int[] distToLeft = new int[height.length];
-      int[] distToRight = new int[height.length];
-      int top = -1;
-      int[] stack = new int[height.length];
+      int[] toLeftBoundary = new int[height.length];
+      Stack<Integer> stack = new Stack<Integer>();
       for (int i = 0; i < height.length; ++i) {
-        while (top != -1 && height[i] <= height[stack[top]]) {
-          --top;
+        while (!stack.isEmpty() && height[stack.peek()] >= height[i]) {
+          stack.pop();
         }
-        distToLeft[i] = top == -1? i : i - stack[top] - 1;
-        stack[++top] = i;
-      }
-      
-      top = -1;
+        toLeftBoundary[i] = stack.size() != 0? i - stack.peek() - 1 : i;
+        stack.push(i);
+      }   
+      stack.clear();
+      int[] toRightBoundary = new int[height.length];
       for (int i = height.length - 1; i >= 0; --i) {
-        while (top != -1 && height[i] <= height[stack[top]]) {
-          --top;
+        while (!stack.isEmpty() && height[stack.peek()] >= height[i]) {
+          stack.pop();
         }
-        distToRight[i] = top == -1? height.length - i - 1 : stack[top] - i - 1;
-        stack[++top] = i;
+        toRightBoundary[i] = stack.size() != 0? stack.peek() - i - 1 : height.length - i - 1;
+        stack.push(i);
       }
       
+      int maxArea = 0;
       for (int i = 0; i < height.length; ++i) {
-        int area = height[i] * (distToLeft[i] + distToRight[i] + 1);
-        if (area > max) {
-          max = area;
-        }
+        maxArea = Math.max(maxArea, (toLeftBoundary[i] + toRightBoundary[i] + 1) * height[i]);
       }
-      
-      return max;
+      return maxArea;
     }
   }
   
