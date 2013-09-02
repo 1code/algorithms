0017c5dc4f67f44549efca92b4cbc3bc0f8cb200
yexi <yexijiang@gmail.com>
2013-09-02 11:02:37 -0400

    improve Four Sum

--- src/main/java/algorithm/lc/FourSum.java
+++ src/main/java/algorithm/lc/FourSum.java
@@ -21,43 +21,59 @@ import java.util.Set;
  */
 // O(n^3) space, O(n^3) time
 public class FourSum {
-  
+
   public class Solution {
     // use two pointers inside a two-level nested loop
     public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-          // Start typing your Java solution below
-          // DO NOT write main() function
-      ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+      // Start typing your Java solution below
+      // DO NOT write main() function
       Arrays.sort(num);
-      Set<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>();
-      
+      ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+
       for (int i = 0; i < num.length - 3; ++i) {
+        if (i > 0 && num[i] == num[i - 1]) {
+          continue;
+        }
         for (int j = i + 1; j < num.length - 2; ++j) {
+          if (j > i + 1 && num[j] == num[j - 1]) {
+            continue;
+          }
           int k = j + 1;
           int l = num.length - 1;
+
           while (k < l) {
             int sum = num[i] + num[j] + num[k] + num[l];
             if (sum == target) {
-              ArrayList<Integer> list = new ArrayList<Integer>();
-              list.add(num[i]);
-              list.add(num[j]);
-              list.add(num[k]);
-              list.add(num[l]);
-              if (set.add(list)) {
-                res.add(list);
+              if (k > j + 1 && num[k] == num[k - 1]) {
+                ++k;
+                continue;
               }
-              ++k;
+              if (l < num.length - 1 && num[l] == num[l + 1]) {
+                --l;
+                continue;
+              }
+              ArrayList<Integer> quad = new ArrayList<Integer>();
+              quad.add(num[i]);
+              quad.add(num[j]);
+              quad.add(num[k]);
+              quad.add(num[l]);
+              res.add(quad);
               --l;
-            }
-            else if (sum > target) {
-              --l;
-            }
-            else {
               ++k;
+            } else if (sum > target) {
+              do {
+                --l;
+              } while (k < l && num[l] == num[l + 1]);
+            } else {
+              do {
+                ++k;
+              } while (k < l && num[k] == num[k - 1]);
             }
           }
+
         }
       }
+
       return res;
     }
   }
