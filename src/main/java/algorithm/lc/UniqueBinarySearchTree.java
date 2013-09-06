c94ca9beca197b17ec22c28c5eaa2686ee7fff28
yexi <yexijiang@gmail.com>
2013-09-06 15:21:51 -0400

    improve Unique Binary Tree

--- src/main/java/algorithm/lc/UniqueBinarySearchTree.java
+++ src/main/java/algorithm/lc/UniqueBinarySearchTree.java
@@ -3,26 +3,23 @@ package algorithm.lc;
 import java.util.Arrays;
 
 /**
- * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
- *
- * For example,
- * Given n = 3, there are a total of 5 unique BST's.
- *
- *  1         3     3      2      1 
- *   \       /     /      / \      \
- *    3     2     1      1   3      2
- *   /     /       \                 \
- *  2     1         2                 3 
- *
+ * Given n, how many structurally unique BST's (binary search trees) that store
+ * values 1...n?
+ * 
+ * For example, Given n = 3, there are a total of 5 unique BST's.
+ * 
+ * 1 3 3 2 1 \ / / / \ \ 3 2 1 1 3 2 / / \ \ 2 1 2 3
+ * 
  */
-// O(n) space, O(n) time
 public class UniqueBinarySearchTree {
-  
-  public class Solution {
-    // recursive plus DP
+
+  public class SolutionRecursive {
+    
+    // O(n) space, O(n^2) time
+ // recursive plus DP
     public int numTrees(int n) {
-          // Start typing your Java solution below
-          // DO NOT write main() function
+      // Start typing your Java solution below
+      // DO NOT write main() function
       if (n <= 1) {
         return 1;
       }
@@ -32,7 +29,7 @@ public class UniqueBinarySearchTree {
       mem[1] = 1;
       return count(n, mem);
     }
-    
+
     private int count(int n, int[] mem) {
       int sum = 0;
       for (int i = 1; i <= n; ++i) {
@@ -46,6 +43,26 @@ public class UniqueBinarySearchTree {
         sum += mem[i - 1] * mem[n - i];
       }
       return sum;
+    }
+  }
+
+  public class SolutionDP {
+    // O(n) space, O(n^2) time
+    // count[i] stores the number of distinct trees with size i
+    public int numTrees(int n) {
+      // Start typing your Java solution below
+      // DO NOT write main() function
+      int[] count = new int[n + 1];
+      count[0] = 1;
+      count[1] = 1;
+
+      for (int i = 2; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) {
+          count[i] += count[j] * count[i - 1 - j];
+        }
+      }
+
+      return count[n];
     }
   }
 
