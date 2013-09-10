810b02ebe719ff8c7e92fd21bf56f27df46fa2ce
yexi <yexijiang@gmail.com>
2013-09-10 12:48:01 -0400

    improve Pascal Triangle II

--- src/main/java/algorithm/lc/PascalTriangleII.java
+++ src/main/java/algorithm/lc/PascalTriangleII.java
@@ -21,11 +21,11 @@ public class PascalTriangleII {
           // Start typing your Java solution below
           // DO NOT write main() function
       ArrayList<Integer> res = new ArrayList<Integer>();
-      for (int i = 0; i < rowIndex; ++i) {
+      for (int i = 0; i <= rowIndex; ++i) {
         res.add(1);
       }
       
-      for (int i = 2; i < rowIndex; ++i) {
+      for (int i = 2; i <= rowIndex; ++i) {
         int upperLeft = res.get(0);
         int upperRight = res.get(1);
         for (int j = 1; j < i; ++j) {
@@ -33,7 +33,6 @@ public class PascalTriangleII {
           upperLeft = upperRight;
           upperRight = res.get(j + 1);
         }
-        System.out.println(res.toString());
       }
       return res;
     }
