6a745c19963a3dbc5f4deed41e2d409b2506cddd
yexi <yexijiang@gmail.com>
2013-09-10 11:38:21 -0400

    improve Pascal Triangle

--- src/main/java/algorithm/lc/PascalTriangle.java
+++ src/main/java/algorithm/lc/PascalTriangle.java
@@ -3,47 +3,37 @@ package algorithm.lc;
 import java.util.ArrayList;
 
 /**
- Given numRows, generate the first numRows of Pascal's triangle.
-
-For example, given numRows = 5,
-Return
-
-[
-     [1],
-    [1,1],
-   [1,2,1],
-  [1,3,3,1],
- [1,4,6,4,1]
-]
+ * Given numRows, generate the first numRows of Pascal's triangle.
+ * 
+ * For example, given numRows = 5, Return
+ * 
+ * [ [1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1] ]
  */
-// O(n^2) space, O(n) time
 public class PascalTriangle {
-  
+
+  // O(n^2) space, O(n^2) time
   public class Solution {
     public ArrayList<ArrayList<Integer>> generate(int numRows) {
-          // Start typing your Java solution below
-          // DO NOT write main() function
+      // Start typing your Java solution below
+      // DO NOT write main() function
       ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
       if (numRows == 0) {
         return res;
       }
-      ArrayList<Integer> first = new ArrayList<Integer>();
-      first.add(1);
-      res.add(first);
+      ArrayList<Integer> prev = new ArrayList<Integer>();
+      prev.add(1);
+      res.add(prev);
       for (int i = 1; i < numRows; ++i) {
-        ArrayList<Integer> prev = res.get(i - 1);
         ArrayList<Integer> cur = new ArrayList<Integer>();
-        for (int j = 0; j < prev.size() + 1; ++j) {
-          if (j == 0 || j == prev.size()) {
-            cur.add(1);
-          }
-          else {
-            cur.add(prev.get(j - 1) + prev.get(j));
-          }
+        cur.add(1);
+        for (int c = 1; c < i; ++c) {
+          cur.add(prev.get(c - 1) + prev.get(c));
         }
+        cur.add(1);
         res.add(cur);
+        prev = cur;
       }
-      
+
       return res;
     }
   }
