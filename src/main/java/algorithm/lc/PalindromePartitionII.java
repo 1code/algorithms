b85b439462c6535e08b6e884cb797129e7e7d2ed
yexi <yexijiang@gmail.com>
2013-09-11 11:53:53 -0400

    improve Palindrome Partition II

--- src/main/java/algorithm/lc/PalindromePartitionII.java
+++ src/main/java/algorithm/lc/PalindromePartitionII.java
@@ -12,28 +12,30 @@ package algorithm.lc;
  */
 public class PalindromePartitionII {
 
-  // DP, res[i] records the min cuts between i and n
+  // DP, res[i] records the min number of partitions between i and n
   // also the palindrome isPalindrome is used to keep track that whether s[i..j] is palindrome
-  class Solution {  
+  public class Solution {  
     public int minCut(String s) {  
       // Start typing your C/C++ solution below  
       // DO NOT write int main() function  
       int n = s.length();  
       int[] res = new int[n + 1]; // keep min number of partitions from in range s[i..n]
-      for(int i = 0 ; i < res.length; ++i){  
-        res[i] = n - i;  
+      for(int i = 0 ; i < res.length; ++i) {  
+        res[i] = n - i;  // initial as maximal possible partitions
       }  
-      boolean[][] isPalindrome = new boolean[n][n];
-      for(int i = n - 1; i >= 0; --i){  
+      boolean[][] isPalindrome = new boolean[n][n]; // use 2D DP to record palindrome
+      for(int i = n - 1; i >= 0; --i) {  
         for(int j = i; j < n ; ++j){  
-          if(s.charAt(i) == s.charAt(j) && (j - i < 2 || isPalindrome[i + 1][j - 1])){  // s[1-j] is palindrome
+          if(s.charAt(i) == s.charAt(j) && (j - i < 2 || isPalindrome[i + 1][j - 1])){  // s[i...j] is palindrome
             isPalindrome[i][j] = true;  
+            // if s[i...j] is palindrome, then maximal partition of s[i...n] is at most 1 + maximal partition of s[j + 1...n]
             res[i] = Math.min(res[i], res[j + 1] + 1);  
           }  
         }  
       }  
       return res[0] - 1;  
     }  
+    
   };  
-
+  
 }
