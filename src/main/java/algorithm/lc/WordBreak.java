9dc04cd39d0b1636f70ac60bbb57b8060fdd0127
yexi <yexijiang@gmail.com>
2013-10-05 14:33:26 -0400

    imporve WordBreak

--- src/main/java/algorithm/lc/WordBreak.java
+++ src/main/java/algorithm/lc/WordBreak.java
@@ -11,55 +11,33 @@ public class WordBreak {
   public class Solution {
     public boolean wordBreak(String s, Set<String> dict) {
           // Note: The Solution object is instantiated only once and is reused by each test case.
-      Map<Pair, Boolean> cache = new HashMap<Pair, Boolean>();
+      Map<String, Boolean> cache = new HashMap<String, Boolean>();
       return found(s, 0, s.length(), dict, cache);     
     }
     
-    private boolean found(String s, int start, int end, Set<String> dict, Map<Pair, Boolean> cache) {
+    private boolean found(String s, int start, int end, Set<String> dict, Map<String, Boolean> cache) {
       if (start >= end) {
         return false;
       }
       // check from memorization
-      Boolean res = cache.get(new Pair(start, end));
+      Boolean res = cache.get(s.substring(start, end));
       if (res != null) {
         return res;
       }
       // calculate from scratch
       if (dict.contains(s.substring(start, end))) {
-        cache.put(new Pair(start, end), true);
+        cache.put(s.substring(start, end), true);
         return true;
       }
       else { 
         for (int i = start + 1; i < end; ++i) {
           if (found(s, start, i, dict, cache) && found(s, i, end, dict, cache)) {
-            cache.put(new Pair(start, end), true);
+            cache.put(s.substring(start, end), true);
             return true;
           }
         }
-        cache.put(new Pair(start, end), false);
+        cache.put(s.substring(start, end), false);
         return false;
-      }
-    }
-    
-    private class Pair {
-      int start;
-      int end;
-      
-      public Pair(int start, int end) {
-        this.start = start;
-        this.end = end;
-      }
-      
-      public boolean equals(Object o) {
-        if (o instanceof Pair) {
-          Pair other = (Pair)o;
-          return this.start == other.start && this.end == other.end;
-        }
-        return false;
-      }
-      
-      public int hashCode() {
-        return this.start * (this.end * 11);
       }
     }
   }
