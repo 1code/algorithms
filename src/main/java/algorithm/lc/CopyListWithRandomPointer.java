4b2bbc23f58d2b00d77d33dfad121de98e66d3df
yexi <yexijiang@gmail.com>
2013-10-05 22:27:49 -0400

    add lc, CopyListWithRandomPointer

---
+++ src/main/java/algorithm/lc/CopyListWithRandomPointer.java
@@ -0,0 +1,50 @@
+package algorithm.lc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CopyListWithRandomPointer {
+
+  static class RandomListNode {
+    int label;
+    RandomListNode next, random;
+
+    RandomListNode(int x) {
+      this.label = x;
+    }
+  };
+
+  public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+          // Note: The Solution object is instantiated only once and is reused by each test case.
+      Map<Integer, RandomListNode> map = new HashMap<Integer, RandomListNode>(100);
+      RandomListNode fakeHead = new RandomListNode(0);
+      fakeHead.next = head;
+      RandomListNode newFakeHead = new RandomListNode(0);
+      RandomListNode curNode = fakeHead;
+      RandomListNode curCopy = newFakeHead;
+      
+      while (curNode.next != null) {
+        curCopy.next = new RandomListNode(curNode.next.label);
+        map.put(curNode.next.label, curCopy.next);
+        curNode = curNode.next;
+        curCopy = curCopy.next;
+      }
+      
+      curNode = fakeHead.next;
+      curCopy = newFakeHead.next;
+      // add random
+      while (curNode != null) {
+        if (curNode.random != null) {
+          curCopy.random = map.get(curNode.random.label);
+        }
+        curCopy = curCopy.next;
+        curNode = curNode.next;
+      }    
+      
+      return newFakeHead.next;
+    }
+    
+  }
+
+}
