ae49b5e87da50334b2a398c6504a6c5511ec63e9
yexi <yexijiang@gmail.com>
2013-10-20 16:05:52 -0400

    improve climbing stairs

--- src/main/java/algorithm/lc/ClimbingStairs.java
+++ src/main/java/algorithm/lc/ClimbingStairs.java
@@ -17,14 +17,11 @@ public class ClimbingStairs {
       // DO NOT write main() function
       if (n == 0) {
         return 0;
-      } else if (n == 1) {
-        return 1;
-      }
+      } 
       int[] ways = new int[n + 1];
-      ways[0] = 0;
+      ways[0] = 1;
       ways[1] = 1;
-      ways[2] = 2;
-      for (int i = 3; i < ways.length; ++i) {
+      for (int i = 2; i < ways.length; ++i) {
         ways[i] = ways[i - 1] + ways[i - 2];
       }
       return ways[n];
